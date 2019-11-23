a72e0d466933702b98a1662d5276a4d4cf967bc4
nadavfima <nadavfima@gmail.com>
2019-11-23 20:07:01 +0200

    Update ReminderNotification.kt

--- src/main/kotlin/com/fimaworks/jetbrains/tenbis/reminder/ReminderNotification.kt
+++ src/main/kotlin/com/fimaworks/jetbrains/tenbis/reminder/ReminderNotification.kt
@@ -8,41 +8,42 @@ import java.time.LocalDateTime
 
 object ReminderNotification {
 
+    // every notification must have a defined group, with an ID, Display Type and log flag
+    private val group = NotificationGroup("10bis Plugin", NotificationDisplayType.STICKY_BALLOON, false)
+
+    private val notificationListener = NotificationListener { notification, event ->
+        // event could be null in some cases
+        if (event != null) {
+            if (event.url != null) {
+                BrowserUtil.browse(event.url)
+            }
+        }
+
+        // dismiss notification whenever clicked something
+        notification?.expire()
+    }
+
     fun notifyUser() {
-        // prep message
-        val message = escapeString(
-            // order button
-            "<a href='${Constants.browserUrl}'>Order Now</a>" +
-                    // six spaces
-                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
-                    // dismiss buton
-                    "<a href='dismiss'>Dismiss</a>"
+
+        // prep title & message
+        val title = "Did you remember to order food?"
+
+        val content = escapeString(
+            "<a href='${Constants.browserUrl}'>Order Now</a>" + // order button
+                    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + // six spaces
+                    "<a href='dismiss'>Dismiss</a>" // dismiss buton
         )!!
 
         // create notificaiton
-        val notification = NotificationGroup("10bis Plugin", NotificationDisplayType.STICKY_BALLOON, false)
-            .createNotification(
-                "Did you remember to order food?",
-                message,
-                NotificationType.WARNING
-            ) { notification, event ->
-                if (event != null) {
-                    if (event.url != null) {
-                        BrowserUtil.browse(event.url)
-                    }
-                }
+        val notification = group.createNotification(title, content, NotificationType.WARNING, notificationListener)
 
-                // dismiss notification
-                notification?.expire()
-
-            }
-
+        // whenever a notification is dismissed
         notification.whenExpired {
             // update last reminder to now
             ReminderPersistentStateComponent.instance.state.lastReminder = LocalDateTime.now()
         }
 
-        // ping user
+        // ping user, not need for project object
         notification.notify(null)
     }
 
